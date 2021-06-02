65270fb08430fb2eb14e1eeff01cae4012285229
victor-singha <vicunite2@gmail.com>
2021-06-02 11:10:17 +0530

    initial commit

---
+++ add.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("Num Addition is: " + c);
+let s1 = "10";
+let s2 = "20";
+let s3 = s1 + s2;
+console.log("Str Addition is: " + s3);
+let d = new Date(); //creates a variable which will contain the current date
+let today =
+  "Today is: " + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+let time = "Time: " + d.getHours() + ":" + d.getMinutes();
+
+console.log(today);
